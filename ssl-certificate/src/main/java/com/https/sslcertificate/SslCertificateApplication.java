994650544f9499f8a084f252a88035fd002dc474
paderewska <iza.paderewska@gmail.com>
2018-10-25 22:35:59 +0200

    first commit - start

---
+++ ssl-certificate/src/main/java/com/https/sslcertificate/SslCertificateApplication.java
@@ -0,0 +1,12 @@
+package com.https.sslcertificate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslCertificateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SslCertificateApplication.class, args);
+	}
+}
